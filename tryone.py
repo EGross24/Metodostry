7e9fb51f8f8b0fc05d6501eaec05050776bfd94e
EGross24 <145200661+EGross24@users.noreply.github.com>
2023-12-02 12:11:12 -0400

    Add files via upload

--- tryone.py
+++ tryone.py
@@ -1,11 +1,21 @@
 
-"""import json
-lista_de_peliculas = []
-
-with open ('movies.txt', 'r') as archivo:
-    contenido = archivo.read()
-    print(contenido)"""
-
-with open("movies.txt", "r", encoding="utf-8") as file:
-    contenido = file.readlines()
-    print(contenido)
+class Persona:
+    def __init__(self):
+        self.nombre = str
+        self.edad = int
+    
+    def crear_personas(self):
+        cantidad = int(input("Cantidad de personas: "))
+        personas = []
+        for idx in range(cantidad):
+            persona = Persona()
+            print(f" ========== PERSONA {idx + 1} ==========")
+            persona.nombre = input("Ingrese el nombre: ")
+            persona.edad = int(input("Ingrese la edad: "))
+            personas.append(persona)
+        self.listar_personas(personas)
+    
+    def listar_personas(self, lista_personas):
+        for persona in lista_personas:
+            print(f"Nombre: {persona.nombre}")
+            print(f"Edad: {persona.edad}")
